429b6bb37832cf7a66b7c8bb52c82d7f4f5bf86f
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-02-20 12:33:03 -0500

    - improved property behavior

--- preql/__init__.py
+++ preql/__init__.py
@@ -2,6 +2,6 @@ from preql.executor import Executor, Dialects
 from preql.parser import parse
 from preql.core.models import Environment
 
-__version__ = "0.0.1-rc.7"
+__version__ = "0.0.1-rc.8"
 
 __all__ = ["parse", "Executor", "Dialects", "Environment"]
