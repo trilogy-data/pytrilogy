6bf5fd65ea27748badc89b1f33bf6cab9c6a4039
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-02-23 22:36:01 -0500

    - fix bigquery quoting for nested struct references

--- preql/__init__.py
+++ preql/__init__.py
@@ -1,7 +1,11 @@
-from preql.core.models import Environment
 from preql.executor import Executor, Dialects
 from preql.parser import parse
+from preql.core.models import Environment
 
-__version__ = "0.0.1-rc.19"
+<<<<<<< HEAD
+__version__ = "0.0.1-rc.14"
+=======
+__version__ = "0.0.1-rc.11"
+>>>>>>> - fix bigquery quoting for nested struct references
 
 __all__ = ["parse", "Executor", "Dialects", "Environment"]
