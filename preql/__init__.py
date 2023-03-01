b12f1085daf72b8e51f13f47743f0112939ea8ab
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-03-01 10:45:50 -0500

    version bump

--- preql/__init__.py
+++ preql/__init__.py
@@ -2,10 +2,7 @@ from preql.executor import Executor, Dialects
 from preql.parser import parse
 from preql.core.models import Environment
 
-<<<<<<< HEAD
-__version__ = "0.0.1-rc.14"
-=======
-__version__ = "0.0.1-rc.11"
->>>>>>> - fix bigquery quoting for nested struct references
+
+__version__ = "0.0.1-rc.15"
 
 __all__ = ["parse", "Executor", "Dialects", "Environment"]
