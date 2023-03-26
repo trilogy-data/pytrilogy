370c301fc72fe005ce0435006e069f1759e31f9e
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-03-26 11:31:16 -0400

    - increase test coverage

--- tests/stack_overflow/test_filtering_reduction.py
+++ tests/stack_overflow/test_filtering_reduction.py
@@ -26,3 +26,24 @@ order by
     env, parsed = parse(QUERY, environment=Environment(working_path=dirname(__file__)))
     select: Select = parsed[-1]
     query = process_query(statement=select, environment=env)
+
+
+def test_filtering_reduction_two():
+    QUERY = """
+import so_concepts.badge as badge;
+import so_concepts.question as question;
+import so_concepts.user as user;
+import so_concepts.answer as answer;
+import so_concepts.user_metrics as user_metrics;
+import so_concepts.tag as tag;
+
+SELECT
+    question.top_ids,
+    answer.comment_count
+order
+    by answer.comment_count desc
+limit 10
+    ;"""
+    env, parsed = parse(QUERY, environment=Environment(working_path=dirname(__file__)))
+    select: Select = parsed[-1]
+    query = process_query(statement=select, environment=env)
