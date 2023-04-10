6ba5c0ec39e8b1fa173b5677312074a96330d7df
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-04-09 21:04:29 -0400

    More fixes to join selection, to properly include namespace is resolution

--- preql/__init__.py
+++ preql/__init__.py
@@ -2,6 +2,6 @@ from preql.core.models import Environment
 from preql.executor import Executor, Dialects
 from preql.parser import parse
 
-__version__ = "0.0.1-rc.23"
+__version__ = "0.0.1-rc.24"
 
 __all__ = ["parse", "Executor", "Dialects", "Environment"]
