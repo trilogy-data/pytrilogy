fa854cb2cbf868eeb06d6da6ac99acb86b036b4f
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-04-15 16:46:58 -0400

    Edickinson min max fixes (#43)
    * Fix min/max function derivation, add in AUTO for property derivation.
    
    * Fix min/max function derivation, add in AUTO for property derivation.

--- preql/__init__.py
+++ preql/__init__.py
@@ -2,6 +2,6 @@ from preql.core.models import Environment
 from preql.executor import Executor, Dialects
 from preql.parser import parse
 
-__version__ = "0.0.1-rc.25"
+__version__ = "0.0.1-rc.26"
 
 __all__ = ["parse", "Executor", "Dialects", "Environment"]
