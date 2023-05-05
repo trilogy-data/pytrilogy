20936455c036bca6cbf604e271584c0121b7bf49
efromvt <ethan.dickinson@gmail.com>
2023-05-05 18:52:19 -0400

    One more grain check

---
+++ tests/complex/test_aggregate_at_grain.py
@@ -0,0 +1,44 @@
+from preql.core.models import Select
+from preql.core.query_processor import process_query
+from preql.dialect.bigquery import BigqueryDialect
+from preql.hooks.query_debugger import DebuggingHook
+from preql.parser import parse
+
+
+def test_select():
+    declarations = """
+key user_id int metadata(description="the description");
+property user_id.display_name string metadata(description="The display name ");
+property user_id.about_me string metadata(description="User provided description");
+
+
+key post_id int;
+metric user_post_count <-count(post_id) by user_id;
+
+
+datasource posts (
+    user_id: user_id,
+    id: post_id
+    )
+    grain (post_id)
+    address bigquery-public-data.stackoverflow.post_history
+;
+
+
+metric avg_user_post_count_double <- avg(user_post_count)*2;
+
+select
+    avg_user_post_count
+;
+
+
+    """
+    env, parsed = parse(declarations)
+    select: Select = parsed[-1]
+
+    query = process_query(statement=select, environment=env, hooks=[DebuggingHook()])
+
+    generator = BigqueryDialect()
+    sql = generator.compile_statement(query)
+    assert "count(cte_posts_at_local_post_id" in sql
+    assert "avg(cte_posts_at_local_post_id_at_local_user_id" in sql
