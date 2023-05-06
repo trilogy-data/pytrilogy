80701a6432239d220c3f7939834bc2fae405bb3c
efromvt <ethan.dickinson@gmail.com>
2023-05-06 13:29:50 -0400

    type fixes

--- preql/parsing/render.py
+++ preql/parsing/render.py
@@ -1,8 +1,16 @@
-from typing import TYPE_CHECKING, Union
 from functools import singledispatchmethod
-from preql.core.models import Select, SelectItem, Concept, ConceptTransform, Function, OrderItem
+
 from jinja2 import Template
+
 from preql.constants import DEFAULT_NAMESPACE
+from preql.core.models import (
+    Concept,
+    ConceptTransform,
+    Function,
+    OrderItem,
+    Select,
+    SelectItem,
+)
 
 QUERY_TEMPLATE = Template(
     """SELECT{%- for select in select_columns %}
@@ -16,41 +24,43 @@ LIMIT {{ limit }}{% endif %};"""
 )
 
 
-class Renderer():
+class Renderer:
     @singledispatchmethod
     def to_string(self, arg):
         raise NotImplementedError("Cannot render type {}".format(type(arg)))
-    
+
     @to_string.register
     def _(self, arg: Select):
         return QUERY_TEMPLATE.render(
-        select_columns=[self.to_string(c) for c in arg.selection],
-        order_by=[self.to_string(c) for c in arg.order_by.items],
-        limit=arg.limit,
-    )
+            select_columns=[self.to_string(c) for c in arg.selection],
+            order_by=[self.to_string(c) for c in arg.order_by.items]
+            if arg.order_by
+            else None,
+            limit=arg.limit,
+        )
 
     @to_string.register
-    def _(self, arg:"SelectItem"):
+    def _(self, arg: "SelectItem"):
         return self.to_string(arg.content)
-    
+
     @to_string.register
-    def _(self, arg:"Concept"):
+    def _(self, arg: "Concept"):
         if arg.namespace == DEFAULT_NAMESPACE:
             return arg.name
         return arg.address
-    
-    @to_string.register
-    def _(self, arg:"ConceptTransform"):
-        return f'{self.to_string(arg.function)}->{arg.output.name}'
 
     @to_string.register
-    def _(self, arg:"Function"):
-        inputs= ','.join(self.to_string(c) for c in arg.inputs)
-        return f'{arg.operator}({inputs})'
-    
+    def _(self, arg: "ConceptTransform"):
+        return f"{self.to_string(arg.function)}->{arg.output.name}"
+
     @to_string.register
-    def _(self, arg:"OrderItem"):
-        return f'{self.to_string(arg.expr)} {arg.order.value}'
+    def _(self, arg: "Function"):
+        inputs = ",".join(self.to_string(c) for c in arg.arguments)
+        return f"{arg.operator}({inputs})"
+
+    @to_string.register
+    def _(self, arg: "OrderItem"):
+        return f"{self.to_string(arg.expr)} {arg.order.value}"
 
 
 def render_query(query: "Select") -> str:
