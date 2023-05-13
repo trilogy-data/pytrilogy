09c090e01bf6cd5fe18f9248051ea58cb3d75100
Ethan Dickinson <ethan.dickinson@gmail.com>
2023-05-13 10:17:08 -0400

    Edickinson fix rendering two (#55)
    * Build out auto-derived concepts
    
    * remove dev files
    
    * Mypy fixes
    
    * Build out auto-derived concepts (#46)
    
    * Build out auto-derived concepts
    
    * remove dev files
    
    * Update gitignore
    
    * version bump
    
    * Mypy fixes
    
    * More mypy
    
    * Build out auto-derived concepts (#46)
    
    * Build out auto-derived concepts
    
    * remove dev files
    
    * Update gitignore
    
    * version bump
    
    * Mypy fixes
    
    * More mypy
    
    * Build out auto-derived concepts
    
    * Build out auto-derived concepts (#46)
    
    * Build out auto-derived concepts
    
    * remove dev files
    
    * Update gitignore
    
    * version bump
    
    * Mypy fixes
    
    * More mypy
    
    * Fix merge issues
    
    * remove vscode files
    
    * rendering method
    
    * type fixes
    
    * revert weird merge issues
    
    * more merge fixes
    
    * fix derived namespaces
    
    * fix derived namespaces
    
    * bump version
    
    * Add in rendering of where clause
    
    * add in rendering of where clause

--- preql/parsing/render.py
+++ preql/parsing/render.py
@@ -10,13 +10,16 @@ from preql.core.models import (
     OrderItem,
     Select,
     SelectItem,
+    WhereClause,
+    Conditional,
+    Comparison,
 )
 
 QUERY_TEMPLATE = Template(
     """SELECT{%- for select in select_columns %}
-    {{ select }},{% endfor %}{% if where %}WHERE
-    {{ where }}
-{% endif %}{%- if order_by %}
+    {{ select }},{% endfor %}{% if where %}
+WHERE
+    {{ where }}{% endif %}{%- if order_by %}
 ORDER BY{% for order in order_by %}
     {{ order }}{% if not loop.last %},{% endif %}
 {% endfor %}{% endif %}{%- if limit is not none %}
@@ -33,11 +36,24 @@ class Renderer:
     def _(self, arg: Select):
         return QUERY_TEMPLATE.render(
             select_columns=[self.to_string(c) for c in arg.selection],
+            where=self.to_string(arg.where_clause),
             order_by=[self.to_string(c) for c in arg.order_by.items]
             if arg.order_by
             else None,
             limit=arg.limit,
         )
+
+    @to_string.register
+    def _(self, arg: "WhereClause"):
+        return f"{self.to_string(arg.conditional)}"
+
+    @to_string.register
+    def _(self, arg: "Conditional"):
+        return f"({self.to_string(arg.left)} {arg.operator.value} {self.to_string(arg.right)})"
+
+    @to_string.register
+    def _(self, arg: "Comparison"):
+        return f"{self.to_string(arg.left)} {arg.operator.value} {self.to_string(arg.right)}"
 
     @to_string.register
     def _(self, arg: "SelectItem"):
@@ -61,6 +77,22 @@ class Renderer:
     @to_string.register
     def _(self, arg: "OrderItem"):
         return f"{self.to_string(arg.expr)} {arg.order.value}"
+
+    @to_string.register
+    def _(self, arg: int):
+        return f"{arg}"
+
+    @to_string.register
+    def _(self, arg: str):
+        return f"'{arg}'"
+
+    @to_string.register
+    def _(self, arg: float):
+        return f"{arg}"
+
+    @to_string.register
+    def _(self, arg: bool):
+        return f"{arg}"
 
 
 def render_query(query: "Select") -> str:
