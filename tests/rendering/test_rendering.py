fd86177e7dd5bd7bcc946cd2b23314de6c273efa
efromvt <ethan.dickinson@gmail.com>
2023-05-13 09:25:36 -0400

    add in rendering of where clause

--- tests/rendering/test_rendering.py
+++ tests/rendering/test_rendering.py
@@ -1,10 +1,34 @@
 from preql.parsing.render import render_query
-from preql.core.models import OrderBy, Ordering, OrderItem, Select
+from preql.core.models import (
+    OrderBy,
+    Ordering,
+    OrderItem,
+    Select,
+    WhereClause,
+    Conditional,
+    Comparison,
+)
+from preql.core.enums import ComparisonOperator, BooleanOperator
 
 
 def test_basic_query(test_environment):
     query = Select(
         selection=[test_environment.concepts["order_id"]],
+        where_clause=WhereClause(
+            conditional=Conditional(
+                left=Comparison(
+                    left=test_environment.concepts["order_id"],
+                    right=123,
+                    operator=ComparisonOperator.EQ,
+                ),
+                right=Comparison(
+                    left=test_environment.concepts["order_id"],
+                    right=456,
+                    operator=ComparisonOperator.EQ,
+                ),
+                operator=BooleanOperator.OR,
+            ),
+        ),
         order_by=OrderBy(
             [
                 OrderItem(
@@ -21,6 +45,8 @@ def test_basic_query(test_environment):
         string_query
         == """SELECT
     order_id,
+WHERE
+    (order_id = 123 or order_id = 456)
 ORDER BY
     order_id asc
 ;"""
