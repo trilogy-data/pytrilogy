f949664aade52b6f2758c2c234f03560d29b3e5f
efromvt <ethan.dickinson@gmail.com>
2023-05-21 19:08:39 -0400

    add in non-where clause test

--- tests/rendering/test_rendering.py
+++ tests/rendering/test_rendering.py
@@ -11,7 +11,34 @@ from preql.core.models import (
 from preql.core.enums import ComparisonOperator, BooleanOperator
 
 
+
 def test_basic_query(test_environment):
+    query = Select(
+        selection=[test_environment.concepts["order_id"]],
+        where_clause=None,
+        order_by=OrderBy(
+            [
+                OrderItem(
+                    expr=test_environment.concepts["order_id"],
+                    order=Ordering.ASCENDING,
+                )
+            ]
+        ),
+    )
+
+    string_query = render_query(query)
+    assert (
+        string_query
+        == """SELECT
+    order_id,
+WHERE
+    (order_id = 123 or order_id = 456)
+ORDER BY
+    order_id asc
+;"""
+    )
+
+def test_full_query(test_environment):
     query = Select(
         selection=[test_environment.concepts["order_id"]],
         where_clause=WhereClause(
@@ -40,7 +67,6 @@ def test_basic_query(test_environment):
     )
 
     string_query = render_query(query)
-    print(string_query)
     assert (
         string_query
         == """SELECT
