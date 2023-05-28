f90d3b02024900687891821ef558dda8699435cd
efromvt <ethan.dickinson@gmail.com>
2023-05-28 13:06:46 -0400

    fix linting

--- preql/core/models.py
+++ preql/core/models.py
@@ -1510,6 +1510,13 @@ class Parenthetical(BaseModel):
     class Config:
         smart_union = True
 
+    def __add__(self, other) -> Union["Parenthetical", "Conditional"]:
+        if other is None:
+            return self
+        elif isinstance(other, (Comparison, Conditional, Parenthetical)):
+            return Conditional(left=self, right=other, operator=BooleanOperator.AND)
+        raise ValueError(f"Cannot add {self.__class__} and {type(other)}")
+
     def __repr__(self):
         return f"({str(self.content)})"
 
