2543d4f3abb6ceade48934ed87244ff19b87ad2e
efromvt <ethan.dickinson@gmail.com>
2023-06-04 11:38:37 -0400

    usability fixes

--- preql/parsing/parse_engine.py
+++ preql/parsing/parse_engine.py
@@ -304,6 +304,8 @@ def unwrap_transformation(
         return input
     elif isinstance(input, AggregateWrapper):
         return input.function
+    elif isinstance(input, FilterItem):
+        return input
     else:
         return Function(
             operator=FunctionType.CONSTANT,
