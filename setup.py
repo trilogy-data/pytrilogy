0a4572ccb65b526c64134ff03e7240b0270e008c
efromvt <ethan.dickinson@gmail.com>
2023-12-03 21:27:19 -0500

    remove scripts

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
         "": ["*.tf", "*.jinja", "py.typed"],
     },
     install_requires=install_requires,
-    scripts=["bin/trilogy"],
+    scripts=[],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
