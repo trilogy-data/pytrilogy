b17cf1f776232f9b7d87431444af2604c21e5abe
efromvt <ethan.dickinson@gmail.com>
2023-12-03 21:27:44 -0500

    demo-part-two

--- setup.py
+++ setup.py
@@ -49,5 +49,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
