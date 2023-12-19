43f01cf6f95853df4e1332d5bfc26914da3e349e
efromvt <ethan.dickinson@gmail.com>
2023-12-19 18:32:03 -0500

    fix constants

--- docs/website/docs/src/.vuepress/components/constants.ts
+++ docs/website/docs/src/.vuepress/components/constants.ts
@@ -1,8 +1,8 @@
 
 let BASE_API_URL ='https://preql-demo-backend-cz7vx4oxuq-uc.a.run.app'
-if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-    // dev
-    BASE_API_URL = 'http://localhost:5000';
+// if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+//     // dev
+//     BASE_API_URL = 'http://localhost:5000';
 
-}
+// }
 export const API_URL= BASE_API_URL;
