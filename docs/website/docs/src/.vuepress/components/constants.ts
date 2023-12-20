d4c88dfdb86c78a30a08096439cd669b20498e5c
efromvt <ethan.dickinson@gmail.com>
2023-12-20 08:00:39 -0500

    Fix to executor method to also persist query

--- docs/website/docs/src/.vuepress/components/constants.ts
+++ docs/website/docs/src/.vuepress/components/constants.ts
@@ -2,7 +2,8 @@
 let BASE_API_URL ='https://preql-demo-backend-cz7vx4oxuq-uc.a.run.app'
 // if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
 //     // dev
-//     BASE_API_URL = 'http://localhost:5000';
+//     
 
 // }
+BASE_API_URL = 'http://localhost:5000';
 export const API_URL= BASE_API_URL;
