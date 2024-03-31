faa842d88984d0d992d325984126fe9caf49aa71
efromvt <ethan.dickinson@gmail.com>
2024-03-31 11:55:05 -0400

    reduce version

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,4 @@
 from preql import Environment
-from preql.core.models import LazyEnvironment
-from pathlib import Path
 
 
 def test_multi_environment():
@@ -23,14 +21,3 @@ const pi <- 3.14;
     )
 
     assert basic.concepts["math.pi"].name == "pi"
-
-
-def test_lazy_import():
-    base = Environment()
-
-    env = LazyEnvironment(
-        load_path=Path(__file__).parent / "stack_overflow/stackoverflow.preql",
-        working_path=Path(__file__).parent / "stack_overflow/",
-    )
-
-    assert len(env.concepts) > len(base.concepts)
