a8590cf356ac9e67dc76b3d9b160420cb21eace4
efromvt <ethan.dickinson@gmail.com>
2024-04-12 21:37:37 -0400

    formatting

--- preql/parsing/parse_engine.py
+++ preql/parsing/parse_engine.py
@@ -545,8 +545,6 @@ class ParseToObjects(Transformer):
 
     def DOUBLE_STRING_CHARS(self, args) -> str:
         return args.value
-    
-
 
     @v_args(meta=True)
     def struct_type(self, meta: Meta, args) -> StructType:
