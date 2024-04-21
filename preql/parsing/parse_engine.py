19c74fb1c0f54b9d9a386703555a7c222bd77a04
efromvt <ethan.dickinson@gmail.com>
2024-04-21 19:29:40 -0400

    stash array types

--- preql/parsing/parse_engine.py
+++ preql/parsing/parse_engine.py
@@ -1199,7 +1199,12 @@ class ParseToObjects(Transformer):
     def index_access(self, meta, args):
         args = self.process_function_args(args, meta=meta)
         return IndexAccess(args)
-
+    
+    @v_args(meta=True)
+    def attr_access(self, meta, args):
+        args = self.process_function_args(args, meta=meta)
+        return AttrAccess(args)
+    
     @v_args(meta=True)
     def attr_access(self, meta, args):
         args = self.process_function_args(args, meta=meta)
