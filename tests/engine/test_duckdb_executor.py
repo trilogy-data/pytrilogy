f111d15121b74699878cf289f1e30c1feca775de
efromvt <ethan.dickinson@gmail.com>
2024-05-23 22:08:40 -0400

    duckdb fixes

--- tests/engine/test_duckdb_executor.py
+++ tests/engine/test_duckdb_executor.py
@@ -5,7 +5,6 @@ from preql.executor import Executor
 from preql.core.models import ShowStatement, Concept
 from preql.core.enums import Purpose, Granularity
 
-
 def test_basic_query(duckdb_engine: Executor, expected_results):
     graph = generate_graph(duckdb_engine.environment)
 
@@ -129,3 +128,28 @@ order by item desc;
     assert (
         'local_fact_items."item" as "item"' in results[0]["__preql_internal_query_text"]
     )
+
+
+def test_rollback(duckdb_engine: Executor, expected_results):
+
+    try:
+        _ = duckdb_engine.execute_raw_sql('select abc')
+    except Exception as e:
+        pass
+
+    results = duckdb_engine.execute_raw_sql('select 1')
+    assert results.fetchall()[0] == (1,)
+
+
+def test_basic(duckdb_engine: Executor):
+    test = """
+  auto today <- current_datetime();
+  
+  select 
+    date_add(today, day, 1)->tomorrow,
+    date_diff(today, today, day)->zero,
+    date_trunc(today, year) -> current_year 
+  ;
+    """
+    results = duckdb_engine.execute_text(test)[0].fetchall()
+    assert len(results[0]) == 3
