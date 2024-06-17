22e6adab3eccf24906946a4ae459baed3d1d01e9
efromvt <ethan.dickinson@gmail.com>
2024-06-16 22:44:34 -0400

    version bump

--- preql/__init__.py
+++ preql/__init__.py
@@ -3,6 +3,6 @@ from preql.dialect.enums import Dialects
 from preql.executor import Executor
 from preql.parser import parse
 
-__version__ = "0.0.1.92"
+__version__ = "0.0.1.93"
 
 __all__ = ["parse", "Executor", "Dialects", "Environment"]
