540f8c7676372d2fb5a42d0bb18983d5434fc1d3
efromvt <ethan.dickinson@gmail.com>
2024-06-21 08:49:06 -0400

    ruff fixes

--- preql/core/processing/node_generators/basic_node.py
+++ preql/core/processing/node_generators/basic_node.py
@@ -4,7 +4,7 @@ from typing import List
 from preql.core.models import (
     Concept,
 )
-from preql.core.processing.nodes import StrategyNode, History, MergeNode, ConstantNode
+from preql.core.processing.nodes import StrategyNode, History, MergeNode
 from preql.core.processing.node_generators.common import (
     resolve_function_parent_concepts,
 )
