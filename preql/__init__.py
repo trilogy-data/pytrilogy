f6e5d9582361ec2b4e9bde8b9355ef598439c5ee
efromvt <ethan.dickinson@gmail.com>
2024-06-22 13:06:40 -0400

    v100

--- preql/__init__.py
+++ preql/__init__.py
@@ -3,6 +3,6 @@ from preql.dialect.enums import Dialects
 from preql.executor import Executor
 from preql.parser import parse
 
-__version__ = "0.0.1.99"
+__version__ = "0.0.1.100"
 
 __all__ = ["parse", "Executor", "Dialects", "Environment"]
