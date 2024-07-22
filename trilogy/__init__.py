c855edc2b7f6e94a0e826768f8c29674bd5aede7
efromvt <ethan.dickinson@gmail.com>
2024-07-22 16:55:47 -0400

    version bump

--- trilogy/__init__.py
+++ trilogy/__init__.py
@@ -4,6 +4,6 @@ from trilogy.executor import Executor
 from trilogy.parser import parse
 from trilogy.constants import CONFIG
 
-__version__ = "0.0.1.111"
+__version__ = "0.0.1.112"
 
 __all__ = ["parse", "Executor", "Dialects", "Environment", "CONFIG"]
--- trilogy/core/optimization.py
+++ trilogy/core/optimization.py
@@ -66,7 +66,7 @@ class InlineDatasource(OptimizationRule):
 
             result = cte.inline_parent_datasource(replaceable, force_group=force_group)
             if result:
-                self.log(f"Ilined parent {replaceable.name}")
+                self.log(f"Inlined parent {replaceable.name}")
             else:
                 self.log(f"Failed to inline {replaceable.name}")
         return optimized
