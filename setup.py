802394b2e035da51683edaff9f6c3a20ef2a3f17
efromvt <ethan.dickinson@gmail.com>
2024-07-26 17:31:49 -0400

    include lark in setup

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
         ]
     ),
     package_data={
-        "": ["*.tf", "*.jinja", "py.typed"],
+        "": ["*.tf", "*.jinja", "py.typed", ".lark"],
     },
     install_requires=install_requires,
     extras_require={
