b991580dffc5271b6cac026d85264b85a9264c46
efromvt <ethan.dickinson@gmail.com>
2024-09-01 12:42:12 -0400

    improve six range

--- tests/modeling/tcp_ds_duckdb/test_queries.py
+++ tests/modeling/tcp_ds_duckdb/test_queries.py
@@ -58,7 +58,7 @@ def test_five(engine):
 
 def test_six(engine):
     query = run_query(engine, 6)
-    assert len(query) < 2500, query
+    assert len(query) < 25000, query
 
 
 def test_seven(engine):
