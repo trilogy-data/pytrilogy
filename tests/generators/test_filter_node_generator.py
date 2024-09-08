d1fea606b14f06259a88a89b186cc4fd580e746d
efromvt <ethan.dickinson@gmail.com>
2024-09-08 08:00:39 -0400

    code coverage

--- tests/generators/test_filter_node_generator.py
+++ tests/generators/test_filter_node_generator.py
@@ -3,8 +3,8 @@ from trilogy.core.processing.concept_strategies_v3 import search_concepts
 from trilogy.core.processing.node_generators.common import (
     resolve_filter_parent_concepts,
 )
-from trilogy.core.models import Environment
-from trilogy.core.enums import PurposeLineage
+from trilogy.core.models import Environment, Comparison
+from trilogy.core.enums import PurposeLineage, ComparisonOperator
 
 
 def test_gen_filter_node_parents(test_environment: Environment, test_environment_graph):
@@ -33,3 +33,28 @@ def test_gen_filter_node(test_environment, test_environment_graph):
         depth=0,
         source_concepts=search_concepts,
     )
+
+
+def test_gen_filter_node_same_concept(test_environment, test_environment_graph):
+
+    conditional = Comparison(
+        left=test_environment.concepts["category_name"],
+        operator=ComparisonOperator.LIKE,
+        right="%abc%",
+    )
+    node = gen_filter_node(
+        concept=test_environment.concepts["product_id"].with_filter(
+            conditional, test_environment
+        ),
+        local_optional=[
+            test_environment.concepts["category_id"].with_filter(
+                conditional, test_environment
+            )
+        ],
+        environment=test_environment,
+        g=test_environment_graph,
+        depth=0,
+        source_concepts=search_concepts,
+    )
+    assert 1 == 0
+    assert node.conditions == conditional
