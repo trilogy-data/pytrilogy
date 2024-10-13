617c51f3e7c13e634a95b04049b59bfa4281e413
efromvt <ethan.dickinson@gmail.com>
2024-10-12 20:46:19 -0400

    values

--- trilogy/__init__.py
+++ trilogy/__init__.py
@@ -4,6 +4,6 @@ from trilogy.executor import Executor
 from trilogy.parser import parse
 from trilogy.constants import CONFIG
 
-__version__ = "0.0.2.21"
+__version__ = "0.0.2.22"
 
 __all__ = ["parse", "Executor", "Dialects", "Environment", "CONFIG"]
