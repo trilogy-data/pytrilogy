ad86659d6f7b01f3feffae530f83448a7dbc5055
efromvt <ethan.dickinson@gmail.com>
2024-10-21 19:50:50 -0400

    version bump

--- trilogy/__init__.py
+++ trilogy/__init__.py
@@ -4,6 +4,6 @@ from trilogy.executor import Executor
 from trilogy.parser import parse
 from trilogy.constants import CONFIG
 
-__version__ = "0.0.2.25"
+__version__ = "0.0.2.26"
 
 __all__ = ["parse", "Executor", "Dialects", "Environment", "CONFIG"]
