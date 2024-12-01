9eb9b02d343188adb16265b3fc59775e85ebff81
Ethan <ethan.dickinson@gmail.com>
2024-12-01 08:18:49 -0500

    publish version check (#210)
    * publish version check
    
    * remove format string
    
    * bump version
    
    * fix formatting
    
    * remove 3.13
    
    ---------
    
    Co-authored-by: efromvt <ethan.dickinson@gmail.com.com>

--- tests/modeling/tpc_ds_duckdb/conftest.py
+++ tests/modeling/tpc_ds_duckdb/conftest.py
@@ -18,9 +18,10 @@ def engine():
         conf=DuckDBConfig(),
     )
     memory = working_path / "memory" / "schema.sql"
+    import_path = working_path / "memory"
     if Path(memory).exists():
         # TODO: Detect if loaded
-        engine.execute_raw_sql(f"IMPORT DATABASE '{working_path / "memory"}';")
+        engine.execute_raw_sql(f"IMPORT DATABASE '{import_path}';")
     results = engine.execute_raw_sql("SHOW TABLES;").fetchall()
     tables = [r[0] for r in results]
     if "store_sales" not in tables:
@@ -29,7 +30,7 @@ def engine():
         INSTALL tpcds;
         LOAD tpcds;
         SELECT * FROM dsdgen(sf=1);
-        EXPORT DATABASE '{working_path / "memory"}' (FORMAT PARQUET);"""
+        EXPORT DATABASE '{import_path}' (FORMAT PARQUET);"""
         )
     yield engine
 
