33c386b49538936cf565f159a586b3ff176befa0
efromvt <ethan.dickinson@gmail.com.com>
2025-02-10 07:36:51 -0500

    Add typing for commits

--- trilogy/engine.py
+++ trilogy/engine.py
@@ -18,6 +18,9 @@ class EngineConnection(Protocol):
     def execute(self, statement: str, parameters: Any | None = None) -> EngineResult:
         pass
 
+    def commit(self):
+        pass
+
 
 class ExecutionEngine(Protocol):
     pass
@@ -40,12 +43,17 @@ class SqlAlchemyResult(EngineResult):
 
 class SqlAlchemyConnection(EngineConnection):
     def __init__(self, connection: Connection):
-        self.connection = connection
+        from sqlalchemy.future import Connection
+
+        self.connection: Connection = connection
 
     def execute(
         self, statement: str, parameters: Any | None = None
     ) -> SqlAlchemyResult:
         return SqlAlchemyResult(self.connection.execute(statement, parameters))
+
+    def commit(self):
+        self.connection.commit()
 
 
 class SqlAlchemyEngine(ExecutionEngine):
