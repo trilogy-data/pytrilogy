8d7dd116f3059d826286b560acd26578d03c6aa7
efromvt <ethan.dickinson@gmail.com.com>
2025-02-16 10:47:38 -0500

    formatting fix

--- trilogy/dialect/duckdb.py
+++ trilogy/dialect/duckdb.py
@@ -55,11 +55,10 @@ DUCKDB_TEMPLATE = Template(
 CREATE OR REPLACE TABLE {{ output.address.location }} AS
 {% endif %}{%- if ctes %}
 WITH {% for cte in ctes %}
-{{cte.name}} as (
-{{cte.statement}}){% if not loop.last %},{% endif %}{% endfor %}{% endif %}
+{{cte.name}} as ({{cte.statement}}){% if not loop.last %},{% endif %}{% endfor %}{% endif %}
 {%- if full_select -%}{{full_select}}
 {%- else -%}{%- if comment -%}
--- {{ comment }}{%- endif -%}
+-- {{ comment }}{%- endif %}
 SELECT
 {%- for select in select_columns %}
     {{ select }}{% if not loop.last %},{% endif %}{% endfor %}
