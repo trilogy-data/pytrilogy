0f7602876cfaa03e7217d7634cb2e32581ee541f
efromvt <ethan.dickinson@gmail.com.com>
2025-04-26 17:11:30 -0400

    fix test handling

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -3,6 +3,7 @@ from pytest import raises
 from trilogy import Dialects
 from trilogy.constants import MagicConstants, Parsing
 from trilogy.core.enums import BooleanOperator, ComparisonOperator, Purpose
+from trilogy.core.exceptions import InvalidSyntaxException
 from trilogy.core.functions import argument_to_purpose, function_args_to_output_purpose
 from trilogy.core.models.author import Comparison
 from trilogy.core.models.core import (
@@ -730,10 +731,36 @@ select [5,6,7,8] as int_array, 4 as scalar
 
 SELECT
     int_array,
-    sum(scalar)->scalar
+    1+2->scalar
 ;
 """
     with raises(ParseError):
+        env, parsed = parse_text(
+            x, parse_config=Parsing(strict_name_shadow_enforcement=True)
+        )
+    x = """
+key scalar int;    
+property scalar.int_array list<int>;
+
+key split <- unnest(int_array);
+
+datasource avalues (
+    int_array: int_array,
+	scalar: scalar
+    ) 
+grain (scalar) 
+query '''(
+select [1,2,3,4] as int_array, 2 as scalar
+union all
+select [5,6,7,8] as int_array, 4 as scalar
+)''';
+
+SELECT
+    int_array,
+    sum(scalar)->scalar
+;
+"""
+    with raises(InvalidSyntaxException):
         env, parsed = parse_text(
             x, parse_config=Parsing(strict_name_shadow_enforcement=True)
         )
