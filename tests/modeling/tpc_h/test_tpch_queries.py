aab6e56814009632da1c5fd197237a2c4f780b37
efromvt <ethan.dickinson@gmail.com.com>
2025-04-26 21:33:30 -0400

    force filter window promotion

--- tests/modeling/tpc_h/test_tpch_queries.py
+++ tests/modeling/tpc_h/test_tpch_queries.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from pytest import raises
 
-from trilogy import parse
+from trilogy import parse, Executor, Dialects
 from trilogy.core.exceptions import InvalidSyntaxException
 from trilogy.core.models.environment import Environment
 
@@ -15,3 +15,17 @@ def test_adhoc02_error():
         text = f.read()
         with raises(InvalidSyntaxException):
             env, queries = parse(text, env)
+
+
+def test_adhoc03():
+    env = Environment(working_path=working_path)
+    with open(working_path / "adhoc03.preql") as f:
+        text = f.read()
+
+    
+
+    engine: Executor = Dialects.DUCK_DB.default_executor(
+        environment=env, hooks=[]
+    )
+
+    results = engine.execute_query(text)
