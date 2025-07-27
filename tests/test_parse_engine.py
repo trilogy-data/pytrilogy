1efc698760116c2c8caf8b9adf907385afd9bce1
Ethan <ethan.dickinson@gmail.com>
2025-07-27 18:33:49 -0400

    Better Errors (#313)
    * better errors
    
    * version bump
    
    ---------
    
    Co-authored-by: efromvt <ethan.dickinson@gmail.com.com>

--- tests/test_parse_engine.py
+++ tests/test_parse_engine.py
@@ -3,6 +3,7 @@ from pytest import raises
 from trilogy.core.exceptions import UndefinedConceptException
 from trilogy.core.models.environment import Environment
 from trilogy.parsing.parse_engine import (
+    ERROR_CODES,
     PARSER,
     InvalidSyntaxException,
     ParseToObjects,
@@ -66,18 +67,45 @@ address fun;
     x.run_second_parse_pass()
 
 
-TEXT2 = """
+def test_from_error():
+    env = Environment()
+    TEXT2 = """
+    const a <- 1;
+
+    select
+        a,
+    FROM a
+    ;
+    """
+    with raises(InvalidSyntaxException) as e:
+        env.parse(TEXT2)
+    assert ERROR_CODES[101] in str(e.value), e.value
+
+
+def test_error_order_by_missing():
+    env = Environment()
+    TEXT2 = """
 const a <- 1;
 
 select
     a,
-FROM a
+order by a
 ;
-"""
-
-
-def test_from_error():
-    env = Environment()
-
-    with raises(InvalidSyntaxException):
+    """
+    with raises(InvalidSyntaxException) as e:
         env.parse(TEXT2)
+    assert ERROR_CODES[210] in str(e.value)
+
+
+def test_alias_error():
+    env = Environment()
+    TEXT2 = """
+    const a <- 1;
+
+    select
+        a+2 fun,
+    ;
+    """
+    with raises(InvalidSyntaxException) as e:
+        env.parse(TEXT2)
+    assert ERROR_CODES[201] in str(e.value), e.value
