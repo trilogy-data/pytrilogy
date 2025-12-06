20654d3f3726e37015e957f872ae6397fa273fcd
efromvt <ethan.dickinson@gmail.com.com>
2025-12-05 19:57:50 -0500

    typo fix

--- _preql_import_resolver/__init__.py
+++ _preql_import_resolver/__init__.py
@@ -1,3 +1,3 @@
-mypfrom ._preql_import_resolver import PyImportResolver, parse_preql_file
+from ._preql_import_resolver import PyImportResolver, parse_preql_file
 
 __all__ = ["PyImportResolver", "parse_preql_file"]
