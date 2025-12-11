c446efa33e9465b0cc08c77c85c7101bc5f8239f
efromvt <ethan.dickinson@gmail.com.com>
2025-12-11 09:14:36 -0500

    refactor fixes

--- trilogy/core/validation/fix.py
+++ trilogy/core/validation/fix.py
@@ -1,52 +1,188 @@
-from collections import defaultdict
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
 from trilogy import Environment, Executor
 from trilogy.authoring import ConceptDeclarationStatement, Datasource
-from trilogy.core.exceptions import (
-    DatasourceColumnBindingData,
-    DatasourceColumnBindingError,
+from trilogy.core.enums import Modifier
+from trilogy.core.exceptions import DatasourceColumnBindingError
+from trilogy.core.models.core import (
+    ArrayType,
+    DataType,
+    MapType,
+    NumericType,
+    StructType,
+    TraitDataType,
 )
+from trilogy.core.models.datasource import ColumnAssignment
 from trilogy.core.validation.environment import validate_environment
 from trilogy.parsing.render import Renderer
 
 
-def rewrite_file_with_errors(
-    statements: list[Any], errors: list[DatasourceColumnBindingError]
-):
-    renderer = Renderer()
-    output = []
-    ds_error_map: dict[str, list[DatasourceColumnBindingData]] = defaultdict(list)
-    concept_error_map: dict[str, list[DatasourceColumnBindingData]] = defaultdict(list)
+@dataclass
+class DatasourceColumnFix:
+    """Represents a fix to apply to a datasource column."""
+
+    datasource_identifier: str
+    column_address: str
+    new_modifiers: list[Modifier] | None = None
+
+
+@dataclass
+class DatasourceReferenceFix:
+    """Represents a fix to merge a datasource column with an imported reference."""
+
+    datasource_identifier: str
+    column_address: str
+    column_alias: str
+    reference_concept_address: str
+    import_alias: str
+
+
+@dataclass
+class ConceptTypeFix:
+    """Represents a fix to update a concept's data type."""
+
+    concept_address: str
+    new_type: DataType | ArrayType | StructType | MapType | NumericType | TraitDataType
+
+
+def update_datasource_column_modifiers(
+    datasource: Datasource, column_address: str, new_modifiers: list[Modifier]
+) -> None:
+    """Generic method to update column modifiers in a datasource."""
+    for col in datasource.columns:
+        if col.concept.address == column_address:
+            col.modifiers = list(set(col.modifiers + new_modifiers))
+
+
+def process_validation_errors(
+    errors: list[DatasourceColumnBindingError],
+) -> tuple[
+    list[DatasourceColumnFix], list[ConceptTypeFix], list[DatasourceReferenceFix]
+]:
+    """Process validation errors and generate a list of fixes to apply."""
+    column_fixes: list[DatasourceColumnFix] = []
+    concept_fixes: list[ConceptTypeFix] = []
+    reference_fixes: list[DatasourceReferenceFix] = []
+
     for error in errors:
         if isinstance(error, DatasourceColumnBindingError):
             for x in error.errors:
-                if error.dataset_address not in ds_error_map:
-                    ds_error_map[error.dataset_address] = []
-                # this is by dataset address
                 if x.is_modifier_issue():
-                    ds_error_map[error.dataset_address].append(x)
-                # this is by column
+                    column_fixes.append(
+                        DatasourceColumnFix(
+                            datasource_identifier=error.dataset_address,
+                            column_address=x.address,
+                            new_modifiers=x.value_modifiers,
+                        )
+                    )
                 if x.is_type_issue():
-                    concept_error_map[x.address].append(x)
+                    concept_fixes.append(
+                        ConceptTypeFix(
+                            concept_address=x.address,
+                            new_type=x.value_type,
+                        )
+                    )
+
+    return column_fixes, concept_fixes, reference_fixes
+
+
+def update_datasource_column_reference(
+    datasource: Datasource, column_address: str, new_concept_address: str
+) -> None:
+    """Update a datasource column to reference a different concept."""
+    from trilogy.core.models.author import ConceptRef
+
+    for i, col in enumerate(datasource.columns):
+        if col.concept.address == column_address:
+            # Create a new ColumnAssignment with the new concept reference
+            new_col = ColumnAssignment(
+                alias=col.alias,
+                concept=ConceptRef(address=new_concept_address),
+                modifiers=col.modifiers,
+            )
+            datasource.columns[i] = new_col
+            break
+
+
+def apply_fixes_to_statements(
+    statements: list[Any],
+    column_fixes: list[DatasourceColumnFix],
+    concept_fixes: list[ConceptTypeFix],
+    reference_fixes: list[DatasourceReferenceFix],
+) -> list[Any]:
+    """Apply the generated fixes to the statement list."""
+    output = []
+
     for statement in statements:
         if isinstance(statement, Datasource):
-            if statement.identifier in ds_error_map:
-                error_cols = ds_error_map[statement.identifier]
-                for col in statement.columns:
-                    if col.concept.address in [x.address for x in error_cols]:
-                        error_col = [
-                            x for x in error_cols if x.address == col.concept.address
-                        ][0]
-                        col.modifiers = list(
-                            set(col.modifiers + error_col.value_modifiers)
-                        )
+            for col_fix in column_fixes:
+                if (
+                    statement.identifier == col_fix.datasource_identifier
+                    and col_fix.new_modifiers
+                ):
+                    update_datasource_column_modifiers(
+                        statement, col_fix.column_address, col_fix.new_modifiers
+                    )
+
+            for ref_fix in reference_fixes:
+                if statement.identifier == ref_fix.datasource_identifier:
+                    update_datasource_column_reference(
+                        statement,
+                        ref_fix.column_address,
+                        ref_fix.reference_concept_address,
+                    )
+
         elif isinstance(statement, ConceptDeclarationStatement):
-            if statement.concept.address in concept_error_map:
-                error_cols = concept_error_map[statement.concept.address]
-                statement.concept.datatype = error_cols[0].value_type
+            for concept_fix in concept_fixes:
+                if statement.concept.address == concept_fix.concept_address:
+                    statement.concept.datatype = concept_fix.new_type
+
         output.append(statement)
+
+    return output
+
+
+def rewrite_file_with_errors(
+    statements: list[Any],
+    errors: list[DatasourceColumnBindingError],
+    additional_reference_fixes: list[DatasourceReferenceFix] | None = None,
+):
+    renderer = Renderer()
+
+    column_fixes, concept_fixes, reference_fixes = process_validation_errors(errors)
+
+    # Add any additional reference fixes provided
+    if additional_reference_fixes:
+        reference_fixes.extend(additional_reference_fixes)
+
+    output = apply_fixes_to_statements(
+        statements, column_fixes, concept_fixes, reference_fixes
+    )
+
+    return renderer.render_statement_string(output)
+
+
+def rewrite_file_with_reference_merges(
+    statements: list[Any], reference_fixes: list[DatasourceReferenceFix]
+) -> str:
+    """
+    Apply reference merge fixes to a file.
+
+    This function is designed to be used when you want to merge datasource column
+    bindings with imported concept references.
+
+    Args:
+        statements: Parsed statements from the file
+        reference_fixes: List of reference merge fixes to apply
+
+    Returns:
+        Modified file content with reference merges applied
+    """
+    renderer = Renderer()
+
+    output = apply_fixes_to_statements(statements, [], [], reference_fixes)
 
     return renderer.render_statement_string(output)
 
