b0911a269f890ec2a43d334a331c743d2c064686
efromvt <ethan.dickinson@gmail.com.com>
2025-12-19 08:54:55 -0500

    state update work

--- trilogy/execution/state/utility.py
+++ trilogy/execution/state/utility.py
@@ -1,19 +1,23 @@
-from trilogy import Environment
+from trilogy import Environment, Executor
 from trilogy.execution.state.state_store import BaseStateStore
 
 
-def get_default_root_assets(env) -> set[str]:
+def get_default_root_assets(env: Environment) -> set[str]:
     # TODO: find all assets that are not updated by persist statements in the entire workspace
     return set()
 
 
 def get_assets_to_update(
-    env: Environment, store: BaseStateStore, root_assets: set[str] | None
-):
+    env: Environment,
+    store: BaseStateStore,
+    executor: Executor,
+    root_assets: set[str] | None,
+) -> None:
     root_assets = root_assets or get_default_root_assets(env)
 
     for root in root_assets:
-        store.watermark_asset(root)
+        datasource = env.datasources[root]
+        store.watermark_asset(datasource, executor)
 
     keys_to_check = [x for x in env.datasources.keys() if x not in root_assets]
 
