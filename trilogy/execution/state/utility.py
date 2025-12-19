0a89aab96f42adb472d73ab7b5ce11b8b0bf9c1f
efromvt <ethan.dickinson@gmail.com.com>
2025-12-19 08:38:04 -0500

    pre type-checking

--- trilogy/execution/state/utility.py
+++ trilogy/execution/state/utility.py
@@ -1,13 +1,15 @@
 from trilogy import Environment
 from trilogy.execution.state.state_store import BaseStateStore
 
-def get_default_root_assets(env)->set[str]:
-    #TODO: find all assets that are not updated by persist statements in the entire workspace
+
+def get_default_root_assets(env) -> set[str]:
+    # TODO: find all assets that are not updated by persist statements in the entire workspace
     return set()
 
 
-
-def get_assets_to_update(env:Environment, store:BaseStateStore, root_assets: set[str] | None):
+def get_assets_to_update(
+    env: Environment, store: BaseStateStore, root_assets: set[str] | None
+):
     root_assets = root_assets or get_default_root_assets(env)
 
     for root in root_assets:
@@ -17,5 +19,3 @@ def get_assets_to_update(env:Environment, store:BaseStateStore, root_assets: set
 
     for key in keys_to_check:
         store.check_datasource_state(env.datasources[key])
-
-
