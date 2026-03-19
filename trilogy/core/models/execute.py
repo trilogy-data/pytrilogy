c279155a02526faca5ece70b0ff93777872dcef2
efromvt <ethan.dickinson@gmail.com.com>
2026-03-19 09:32:01 -0400

    merge-union-ctes

--- trilogy/core/models/execute.py
+++ trilogy/core/models/execute.py
@@ -1112,6 +1112,17 @@ class UnionCTE:
     def __add__(self, other):
         if not isinstance(other, UnionCTE) or not other.name == self.name:
             raise SyntaxError("Cannot merge union CTEs")
+        extra = [c for c in other.output_columns if c not in self.output_lcl]
+        if not extra:
+            return self
+        self.output_columns = unique(self.output_columns + other.output_columns, "address")
+        # Merge each arm of the union so every branch projects the combined columns.
+        self_by_name = {cte.name: i for i, cte in enumerate(self.internal_ctes)}
+        for other_cte in other.internal_ctes:
+            if other_cte.name in self_by_name:
+                self.internal_ctes[self_by_name[other_cte.name]] = (
+                    self.internal_ctes[self_by_name[other_cte.name]] + other_cte
+                )
         return self
 
 
