20989459eadd5900eebf5810637dc2846a62fbf5
efromvt <ethan.dickinson@gmail.com.com>
2026-03-22 22:17:29 -0400

    fixes

--- trilogy/scripts/serve.py
+++ trilogy/scripts/serve.py
@@ -158,7 +158,6 @@ def create_app(
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
