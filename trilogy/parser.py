83bd160a7681e100d743fae5bb6a96a0461d58e8
efromvt <ethan.dickinson@gmail.com.com>
2026-04-12 06:45:15 -0400

    parser+v1

--- trilogy/parser.py
+++ trilogy/parser.py
@@ -1,6 +1,7 @@
+from pathlib import Path
 from typing import Optional
 
-from trilogy.constants import CONFIG, ParserVersion
+from trilogy.constants import CONFIG, ParserVersion, Parsing
 from trilogy.core.models.environment import Environment
 
 
@@ -11,13 +12,26 @@ def parse(
 
 
 def parse_text(
-    input: str, environment: Optional[Environment] = None
+    input: str,
+    environment: Optional[Environment] = None,
+    root: Path | None = None,
+    parse_config: Parsing | None = None,
 ) -> tuple[Environment, list]:
     if CONFIG.parser_version == ParserVersion.V2:
         from trilogy.parsing.parse_engine_v2 import parse_text as parse_text_v2
 
-        return parse_text_v2(input, environment=environment)
+        return parse_text_v2(
+            input,
+            environment=environment,
+            root=root,
+            parse_config=parse_config,
+        )
 
     from trilogy.parsing.parse_engine import parse_text as parse_text_v1
 
-    return parse_text_v1(input, environment=environment)
+    return parse_text_v1(
+        input,
+        environment=environment,
+        root=root,
+        parse_config=parse_config,
+    )
