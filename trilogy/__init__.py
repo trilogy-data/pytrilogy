0075b63c66deaa8451c274e8a5a062971534dba2
efromvt <ethan.dickinson@gmail.com.com>
2026-04-16 07:35:43 -0400

    additional_query_perf

--- trilogy/__init__.py
+++ trilogy/__init__.py
@@ -4,7 +4,7 @@ from trilogy.dialect.enums import Dialects
 from trilogy.executor import Executor
 from trilogy.parser import parse
 
-__version__ = "0.3.227"
+__version__ = "0.3.228"
 
 __all__ = [
     "parse",
