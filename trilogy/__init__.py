41cdd3093abece7f2a0a46450dc5b86cabf1bdee
efromvt <ethan.dickinson@gmail.com.com>
2026-04-16 20:42:58 -0400

    grammar-fix

--- trilogy/__init__.py
+++ trilogy/__init__.py
@@ -4,7 +4,7 @@ from trilogy.dialect.enums import Dialects
 from trilogy.executor import Executor
 from trilogy.parser import parse
 
-__version__ = "0.3.228"
+__version__ = "0.3.229"
 
 __all__ = [
     "parse",
