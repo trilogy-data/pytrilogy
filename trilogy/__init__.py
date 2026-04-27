47b14e437cf719f0003cc29d8f0aabcb52f7a52a
efromvt <ethan.dickinson@gmail.com.com>
2026-04-26 20:35:30 -0400

    init_bump

--- trilogy/__init__.py
+++ trilogy/__init__.py
@@ -4,7 +4,7 @@ from trilogy.dialect.enums import Dialects
 from trilogy.executor import Executor
 from trilogy.parser import parse
 
-__version__ = "0.3.240"
+__version__ = "0.3.241"
 
 __all__ = [
     "parse",
