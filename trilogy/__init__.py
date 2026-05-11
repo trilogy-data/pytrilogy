46cd49d527c9515979235bb4d114955bae1e87b2
efromvt <ethan.dickinson@gmail.com>
2026-05-11 07:59:37 -0400

    union_sorce_improvement

--- trilogy/__init__.py
+++ trilogy/__init__.py
@@ -4,7 +4,7 @@ from trilogy.dialect.enums import Dialects
 from trilogy.executor import Executor
 from trilogy.parser import parse
 
-__version__ = "0.3.258"
+__version__ = "0.3.259"
 
 __all__ = [
     "parse",
