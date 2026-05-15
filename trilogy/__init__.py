a5e4d50c670f94a3a0dba901a334b44c378744a9
efromvt <ethan.dickinson@gmail.com>
2026-05-15 10:49:45 -0400

    private_rowset_fix

--- trilogy/__init__.py
+++ trilogy/__init__.py
@@ -4,7 +4,7 @@ from trilogy.dialect.enums import Dialects
 from trilogy.executor import Executor
 from trilogy.parser import parse
 
-__version__ = "0.3.262"
+__version__ = "0.3.263"
 
 __all__ = [
     "parse",
